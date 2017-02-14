508f6475684d73cdd44af53bc87570bb92da08a1
HogDogTheGod <throweymcthrowerson@gmail.com>
2017-02-13 21:15:35 -0500

    heroku initial commit

--- app/configure/routes.js
+++ app/configure/routes.js
@@ -10,4 +10,21 @@ module.exports = function(app, passport) {
     .get(function(req, res) {
       res.sendFile(path.join(base, 'public', 'views', 'all_polls.html'));
     })
+
+// login / logout / passport stuff
+  app.get(
+    '/auth/github', 
+    passport.authenticate('github')
+  );
+  app.get(
+    '/auth/github/callback',
+    passport.authenticate('github', {
+      successRedirect: '/',
+      failureRedirect: '/login_failed'
+    })
+  );
+  app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('logged_out')
+  });
 }
